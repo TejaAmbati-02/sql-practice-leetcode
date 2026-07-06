551896e38b346c4b632724d77f924d93927303f3
Teja <tejaambati20@gmail.com>
2026-07-06 19:38:33 +0530

    fix: qualify column name in SQL query for clarity

--- 4_leetcode_183.sql
+++ 4_leetcode_183.sql
@@ -1,6 +1,6 @@
 -- Write your MySQL query statement below
 
-SELECT name AS Customers
+SELECT C.name AS Customers
 FROM Customers C LEFT JOIN Orders O 
 ON O.customerId = C.id
 WHERE O.customerId is NULL;
