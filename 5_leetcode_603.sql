dac09cc918cfb7e4b24781ed22d3c269f931da3f
Teja <tejaambati20@gmail.com>
2026-07-08 21:22:43 +0530

    add SQL script for Leetcode problem 603

---
+++ 5_leetcode_603.sql
@@ -0,0 +1,35 @@
+SHOW DATABASES;
+
+USE leetcode_practice;
+
+
+CREATE TABLE Cinema (
+    seat_id INT NOT NULL AUTO_INCREMENT,
+    free BIT,
+    PRIMARY KEY (seat_id)
+);
+
+INSERT INTO Cinema (free)
+VALUES
+(1),
+(0),
+(1),
+(1),
+(1);
+
+
+SELECT * FROM `Cinema`;
+
+
+-- 0 -> Occupied, 1 -> Free
+
+
+-- Consecutive availble seats in cinema
+
+
+
+
+SELECT seat_id FROM 
+  (SELECT seat_id, free, LEAD(free, 1) OVER(ORDER BY seat_id) AS next_seat
+FROM `Cinema`) Cinema_next_row
+WHERE next_seat = 1;
