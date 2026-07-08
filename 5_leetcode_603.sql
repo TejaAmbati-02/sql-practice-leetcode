fc9ce305b42a729c9342d866a90552e1a16a68f9
Teja <tejaambati20@gmail.com>
2026-07-08 21:36:52 +0530

    fix: add SQL query to handle last row case for consecutive available seats

--- 5_leetcode_603.sql
+++ 5_leetcode_603.sql
@@ -34,3 +34,11 @@ SELECT seat_id FROM
 FROM Cinema) Cinema_next_row
 WHERE (next_seat = 1 OR next_seat IS NULL) AND free = 1
 ORDER BY seat_id;
+
+
+-- Replace the last value of LEAD with 1 to handle the last row case, as it will return NULL for the last row, and we want to consider it as free.
+SELECT seat_id FROM 
+  (SELECT seat_id, free, LEAD(free, 1, 1) OVER(ORDER BY seat_id) AS next_seat
+FROM Cinema) Cinema_next_row
+WHERE (next_seat = 1 OR next_seat IS NULL) AND free = 1
+ORDER BY seat_id;
