20a21043c9c86af77f222622ef3252a1c8d00aec
Teja <tejaambati20@gmail.com>
2026-07-08 22:33:16 +0530

    fix: remove unnecessary alias from customer_number in SQL queries

--- 8_leetcode_586.sql
+++ 8_leetcode_586.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT customer_number AS customer_number 
+SELECT customer_number
     FROM(
         SELECT COUNT(order_number) AS customer_number_orders, customer_number 
         FROM Orders
@@ -8,3 +8,29 @@ SELECT customer_number AS customer_number
 ORDER BY customer_number_orders DESC
 LIMIT 1;
 
+
+
+
+
+WITH cte AS
+(
+    SELECT COUNT(order_number) AS customer_number_orders, customer_number 
+    FROM Orders
+    GROUP BY customer_number
+)
+SELECT customer_number
+FROM cte
+ORDER BY customer_number_orders DESC
+LIMIT 1;
+
+
+
+WITH cte AS
+(
+    SELECT COUNT(order_number) AS customer_number_orders, customer_number 
+    FROM Orders
+    GROUP BY customer_number
+)
+SELECT customer_number
+FROM cte
+WHERE customer_number_orders = (SELECT MAX(customer_number_orders) FROM cte);
