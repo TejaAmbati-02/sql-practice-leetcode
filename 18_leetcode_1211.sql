fc5a86a14993aa590103ef98029fe3c1c1854592
Teja <tejaambati20@gmail.com>
2026-07-13 11:16:24 +0530

    feat: update SQL queries for LeetCode problems 1050 and 1113, and add query for problem 1211

---
+++ 18_leetcode_1211.sql
@@ -0,0 +1,18 @@
+-- # Write your MySQL query statement below
+WITH cte AS
+(SELECT query_name,rating / position AS ratio, 
+CASE WHEN rating < 3 THEN 1 ELSE 0 END AS quality_binary
+FROM Queries)
+SELECT query_name, 
+    ROUND(AVG(ratio), 2) AS quality,
+    ROUND((SUM(quality_binary) / COUNT(*))* 100, 2) AS poor_query_percentage
+FROM cte
+GROUP BY query_name;
+
+
+
+select query_name,
+    round(avg(rating/position),2) as quality,
+    round(count(case when rating < 3 then 1 end)*100.0/count(*),2) as poor_query_percentage
+from Queries
+group by query_name;
